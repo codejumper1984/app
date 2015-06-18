4a713471b82011998b4e4bd4979a5a2eef2070ca
codejumper1984 <mbwangs@163.com>
2015-06-18 17:57:35 +0800

    network
    network

---
+++ network-csharp/NetWork/NetWork/SafeCircleList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetWork
+{
+    public class SafeCircleList<T>
+    {
+        const int DEFAULT_LIST_SIZE = 8;
+
+        public SafeCircleList(int nListSize = DEFAULT_LIST_SIZE)
+        {
+            buffer = new T[nListSize];
+            nFrontIdx = 0;
+            nEndIdx = 0;
+            this.nListSize = nListSize;
+        }
+
+        public bool IsEmpty()
+        {
+            return nFrontIdx == nEndIdx;
+        }
+
+        public bool IsFull()
+        {
+            return (nEndIdx + 1) % nListSize == nFrontIdx;
+        }
+
+        public T Front()
+        {
+            if (!IsEmpty())
+                return buffer[nFrontIdx];
+            return default(T);
+        }
+
+        public T End()
+        {
+            if (!IsEmpty())
+                return buffer[nEndIdx - 1];
+            return default(T);
+        }
+
+        public bool Push(T t)
+        {
+            if(!IsFull())
+            {
+                buffer[nEndIdx] = t;
+                IncEnd();
+                return true;
+            }
+            return false;
+        }
+
+        private void IncEnd()
+        {
+            nEndIdx = (nEndIdx + 1) % nListSize;
+        }
+
+        private void IncFront()
+        {
+            nFrontIdx = (nFrontIdx + 1) % nListSize;
+        }
+
+        // cirle datas by array
+        T[] buffer = null;
+
+        // beginning index of the Data
+        int nFrontIdx;
+
+        // index after end index of the Data
+        int nEndIdx;
+
+        // data size
+        int nListSize;
+    }
+}
---
+++ network-csharp/NetWork/NetWork/TcpSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace NetWork
+{
+    class TcpSession
+    {
+        Socket sSocket = null;
+        String strIPAddress = String.Empty;
+        int nPort;
+        public bool Connect()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse(strIPAddress);
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, nPort);
+            socket.Connect(endPoint);
+
+            return true;
+        }
+    }
+}
