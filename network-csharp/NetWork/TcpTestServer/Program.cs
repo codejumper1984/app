4ea695cb9b2e3a3f42590411fe093ed7d5ba3b19
Codejumper <mbwangs@163.com>
2015-06-26 00:03:47 +0800

    Server Code

--- network-csharp/NetWork/TcpTestServer/Program.cs
+++ network-csharp/NetWork/TcpTestServer/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NetWork;
+using System.Net.Sockets;
+using System.Net;
 
 namespace TcpTestServer
 {
@@ -13,6 +15,15 @@ namespace TcpTestServer
             TcpSession session = TcpSessionManager.CreateSession();
             session.Connect("127.0.0.1", 40001);
             String strLine = Console.ReadLine();
+        }
+        const ushort server_port = 40001;
+        const string server_ip = "127.0.0.1";
+        static void RunServer()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Parse(server_ip), server_port));
+            socket.Listen();
+            socket.Accept();
         }
     }
 }
